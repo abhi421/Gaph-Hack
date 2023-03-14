b03b94abee0620b4b3581cd4d073e38903858bfc
Abhinay Yadav <abhinay.yadav@benjaminmoore.com>
2023-03-15 01:16:40 +0530

    Get a chat and all its members

---
+++ GraphHack/GraphHack/Program.cs
@@ -0,0 +1,18 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+
+var scopes = new[] { "User.Read" };
+var interactiveBrowserCredentialOptions = new InteractiveBrowserCredentialOptions
+{
+    ClientId = "CLIENT_ID"
+};
+var tokenCredential = new InteractiveBrowserCredential(interactiveBrowserCredentialOptions);
+
+var graphClient = new GraphServiceClient(tokenCredential, scopes);
+
+var result = await graphClient.Chats["{chat-id}"].GetAsync((requestConfiguration) =>
+{
+    requestConfiguration.QueryParameters.Expand = new string[] { "members" };
+});
+
+Console.WriteLine(result);
